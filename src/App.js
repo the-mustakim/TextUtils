7dc646129dd901ac75a6cf2808eed8d2802d3594
Mustakim Sayyed <mustakimsayyed50@gmail.com>
2023-01-23 23:31:14 +0530

    Upload
    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+//import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+//import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
+
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert] =useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert(
+      {
+        msg : message,
+        type:type
+      }
+    )
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+
+  const toggleMode=()=>
+  {
+    if(mode==='light')
+    {
+      setMode('dark');
+      document.body.style.background='#020a17';
+      showAlert("Dark Mode has been Enabled","Success")
+      document.title="TextUtils-Dark Mode"
+    }
+    else
+    {
+      setMode('light')
+      document.body.style.background="white";
+      showAlert("Light Mode has been Enabled","Success")
+      document.title="TextUtils-Light Mode"
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggle={toggleMode}/>
+      <Alert alert={alert}/> 
+      <div className="container my-3">
+      {/* <Switch> */}
+          {/* <Route exact path="/about" component={About}>  */} */}
+              {/* <About/> */}
+          {/* </Route>
+          <Route exact path="/" component={TextForm}>   */}
+              <TextForm heading="Enter the Text To  Analyze" mode={mode} showAlert={showAlert}></TextForm>
+          {/* </Route> */}
+      {/* </Switch> */}
+      </div>
+  </>
   );
 }
-
-export default App;
+ 
+export default App; 
---
+++ src/components/TextForm.js
@@ -0,0 +1,117 @@
+//Shortcut for bolier plate code of react functinal based component: rfc
+
+import React, { useState } from "react";
+
+//{useState} : useState Hooks helps us to create the state variable
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    //console.log("UpperCase was Cliked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Text to Upper Case","Success")
+  };
+
+  const handleLowClick = () => {
+    let newTask = text.toLowerCase();
+    setText(newTask);
+    props.showAlert("Text to Lower Case","Success")
+  };
+
+  const handleClear = () => {
+    let newTask = "";
+    setText(newTask);
+    props.showAlert("Cleared","Success")
+  };
+
+  const handleInvertCase = () => {
+    let newText = "";
+    for (let i in text) {
+      //debugger;
+      //console.log("Before"+newText);
+      //console.log(text[i])
+      if (text[i] === text[i].toLowerCase()) {
+        //newText.concat(text[i].toUpperCase());
+        console.log(text[i]);
+        newText = newText + text[i].toUpperCase();
+        //debugger;
+      } else {
+        //console.log(text[i].toLowerCase())
+        newText = newText + text[i].toLowerCase();
+      }
+      
+    }
+    console.log(newText);
+    setText(newText);
+    props.showAlert("Invert Text","Success")
+  };
+
+  const handleCopy = () => {
+    console.log("I am Copy");
+    navigator.clipboard.writeText(text);
+    props.showAlert("Text Copied","Success")
+  };
+
+  // const handleExtraSpace=()=>{
+  //     let
+  // }
+
+  //Event Handler For ONCHANGE: Set Ki gai Value ko change karna is an event so, we have to handle that event
+  //But, event.target: The target event property returns the element that triggered the event
+  // event.target.value: The Value of that element that triggred the event
+  //Agar ham ye event.target.value setText nahi karenge to ham existing value ko change ya phir remove nahi kar payenge
+
+  const handleOnChange = (event) => {
+    //console.log("Handle On Change")
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter the Text Here");
+
+  //text="Nothing"  //Wrong way of updating the text
+  //setText("Enter the Wrong Text Here")  //Correct Way of Updating
+
+  return (
+    <>
+      <h1 style={{ color: props.mode === 'dark' ? 'white' : 'black'}}>{props.heading}</h1>
+      <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black'}}>
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            style={{ background: props.mode === 'dark' ? '#23282f' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}}
+            value={text}
+            onChange={handleOnChange}
+            id="exampleFormControlTextarea1"
+            rows="8"></textarea>
+        </div>
+        <br/>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to UPPERCASE
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleInvertCase}>
+          Convert to InvertCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        {/*<button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>*/}
+        <button className="btn btn-primary mx-1" onClick={handleClear}>
+          Clear
+        </button>
+      </div>
+      <div className="container my-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
+        <h1>Your text Summary</h1>
+        <p>
+          {" "}
+          {text.split(" ").length} Words and {text.length} Charectars
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
